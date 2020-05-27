f523b168bab3c4ce038188ee0ad2247342d2fee9
Justin Powers <contact@justinpowers.com>
2020-05-27 12:00:29 -0500

    Setup essential static structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
 import React from 'react';
 import './App.css';
 
+const searchResults = [
+  {
+    id: 0,
+    title: 'Another Love Song',
+    artist: 'Sappy Sammy',
+    album: 'Songs to Die For',
+  },
+  {
+    id: 1,
+    title: 'Sports! Sports! Sports!',
+    artist: 'Long-Haired Lars',
+    album: 'Selling Out',
+  },
+  {
+    id: 2,
+    title: 'My Country',
+    artist: 'Tommy Twang',
+    album: 'I Lost My...',
+  },
+];
+
+const playlistTracks = [
+  {
+    id: 3,
+    title: 'Hippity Hop',
+    artist: 'Lil B.I.G.',
+    album: 'Beats and More Beats',
+  },
+  {
+    id: 4,
+    title: 'Loopy Lullabies',
+    artist: 'Icelandic Idaho',
+    album: 'Air Conditioners and Other Sounds',
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Spotify Genius!!!</h1>
       </header>
+      <main>
+        <div className="SearchBar">
+          <input type="text" placeholder="Search lyrics..." />
+          <button className="SearchButton" type="button">
+            Search
+          </button>
+        </div>
+        <div className="SearchResults">
+          <h2>Search Results</h2>
+          <ul className="TrackList">
+            {searchResults.map((track) => (
+              <li key={track.id}>
+                &quot;{track.title}&quot; - {track.artist}, <i>{track.album}</i>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="Playlist">
+          <h2>Playlist In Progress</h2>
+          <ul className="TrackList">
+            {playlistTracks.map((track) => (
+              <li key={track.id}>
+                &quot;{track.title}&quot; - {track.artist}, <i>{track.album}</i>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+      <footer />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders essential structure of spotify genius', () => {
+  const { getByText, getByPlaceholderText } = render(<App />);
+
+  const titleHeader = getByText(/Spotify Genius!!!/);
+  expect(titleHeader).toBeInTheDocument();
+
+  const searchField = getByPlaceholderText(/Search lyrics.../);
+  expect(searchField).toBeInTheDocument();
+
+  const searchButton = screen.getByRole('button');
+  expect(searchButton).toBeInTheDocument();
+
+  const searchResultsHeader = getByText(/Search Results/);
+  expect(searchResultsHeader).toBeInTheDocument();
+
+  const playlistInProgressHeader = getByText(/Playlist In Progress/);
+  expect(playlistInProgressHeader).toBeInTheDocument();
 });
